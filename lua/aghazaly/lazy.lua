54b649d5394bfd57c88f70c14c9f73e659376d58
Abdelrahman <ghazaly.abdelrahman@gmail.com>
2024-08-09 18:31:38 +0300

    Revamped the lsp config
    Added dart lsp configs from
    https://www.youtube.com/@RobertBrunhage

--- lua/aghazaly/lazy.lua
+++ lua/aghazaly/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup( { { import = "aghazaly.plugins" } , { import = "aghazaly.flutter-config" } , { import = "aghazaly.lsp"} }, {
+require("lazy").setup({{ import = "aghazaly.plugins"}, {import = "aghazaly.lsp"} } ,  {
   chekcer = {
     enabled = true,
     notify = false,
