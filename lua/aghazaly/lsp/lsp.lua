65843951c27aa677ad0ccd411e165523e59aaf79
Abdelrahman Ghazaly <aghazaly@Abdelrahmans-Laptop.local>
2024-09-07 12:12:37 +0300

    Added auto-session

--- lua/aghazaly/lsp/lsp.lua
+++ lua/aghazaly/lsp/lsp.lua
@@ -1,152 +1,150 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		-- bridges the gap between mason and lspconfig
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lsp_config = require("lspconfig")
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {},
-			})
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    -- bridges the gap between mason and lspconfig
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lsp_config = require("lspconfig")
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+      })
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<leader>dl", vim.diagnostic.setqflist)
 
-			vim.keymap.set({ "n", "i" }, "<C-b>", function()
-				vim.lsp.inlay_hint(0, nil)
-			end)
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>dl", vim.diagnostic.setqflist)
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
+      vim.keymap.set({ "n", "i" }, "<C-b>", function()
+        vim.lsp.inlay_hint(0, nil)
+      end)
 
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-				end,
-			})
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          local keymap = vim.keymap -- for conciseness
 
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = false,
-			})
+          -- set keybinds
+          opts.desc = "Show LSP references"
+          keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-			local dartExcludedFolders = {
-				vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
-				vim.fn.expand("$HOME/.pub-cache"),
-				vim.fn.expand("/opt/homebrew/"),
-				vim.fn.expand("C:/tools/flutter/"),
-			}
+          opts.desc = "Go to declaration"
+          keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-			lsp_config["dcmls"].setup({
-				capabilities = capabilities,
-				cmd = {
-					"dcm",
-					"start-server",
-				},
-				filetypes = { "dart", "yaml" },
-				settings = {
-					dart = {
-						analysisExcludedFolders = dartExcludedFolders,
-					},
-				},
-			})
+          opts.desc = "Show LSP definitions"
+          keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-			lsp_config["dartls"].setup({
-				capabilities = capabilities,
-				cmd = {
-					"dart",
-					"language-server",
-					"--protocol=lsp",
-					-- "--port=8123",
-					-- "--instrumentation-log-file=/Users/robertbrunhage/Desktop/lsp-log.txt",
-				},
-				filetypes = { "dart" },
-				init_options = {
-					onlyAnalyzeProjectsWithOpenFiles = false,
-					suggestFromUnimportedLibraries = true,
-					closingLabels = true,
-					outline = false,
-					flutterOutline = false,
-				},
-				settings = {
-					dart = {
-						analysisExcludedFolders = dartExcludedFolders,
-						updateImportsOnRename = true,
-						completeFunctionCalls = true,
-						showTodos = true,
-					},
-				},
-			})
+          opts.desc = "Show LSP implementations"
+          keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-			lsp_config.astro.setup({
-				capabilities = capabilities,
-			})
+          opts.desc = "Show LSP type definitions"
+          keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-			lsp_config.tailwindcss.setup({
-				capabilities = capabilities,
-			})
+          opts.desc = "See available code actions"
+          keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-			lsp_config.tsserver.setup({
-				capabilities = capabilities,
-			})
+          opts.desc = "Smart rename"
+          keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-			lsp_config.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						-- runtime = {
-						--   version = "LuaJIT",
-						-- },
-						diagnostics = {
-							globals = { "vim" },
-						},
-						-- workspace = {
-						--   checkThirdParty = false,
-						--   library = {
-						--     '${3rd}/luv/library',
-						--     unpack(vim.api.nvim_get_runtime_rile("", true)),
-						--     vim.api.nvim_get_proc,
-						--   }
-						-- },
-					},
-				},
-			})
+          opts.desc = "Show buffer diagnostics"
+          keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-			-- Tooltip for the lsp in bottom right
-			require("fidget").setup({})
+          opts.desc = "Show line diagnostics"
+          keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-			-- Hot reload :)
-			require("dart-tools")
-		end,
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
+          opts.desc = "Go to previous diagnostic"
+          keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-			{ "j-hui/fidget.nvim", tag = "legacy" },
-			-- support for dart hot reload on save
-			"RobertBrunhage/dart-tools.nvim",
-		},
-	},
+          opts.desc = "Go to next diagnostic"
+          keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+          opts.desc = "Show documentation for what is under cursor"
+          keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+          opts.desc = "Restart LSP"
+          keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+        end,
+      })
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = false,
+      })
+
+      local dartExcludedFolders = {
+        vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
+        vim.fn.expand("$HOME/.pub-cache"),
+        vim.fn.expand("/opt/homebrew/"),
+        vim.fn.expand("$HOME/fvm/"),
+        vim.fn.expand("C:/tools/flutter/"),
+      }
+
+      lsp_config["dartls"].setup({
+        capabilities = capabilities,
+        cmd = {
+          "dart",
+          "language-server",
+          "--protocol=lsp",
+          -- "--port=8123",
+          -- "--instrumentation-log-file=/Users/robertbrunhage/Desktop/lsp-log.txt",
+        },
+        filetypes = { "dart" },
+        init_options = {
+          onlyAnalyzeProjectsWithOpenFiles = false,
+          suggestFromUnimportedLibraries = true,
+          closingLabels = true,
+          outline = false,
+          flutterOutline = false,
+        },
+        settings = {
+          dart = {
+            analysisExcludedFolders = dartExcludedFolders,
+            updateImportsOnRename = true,
+            completeFunctionCalls = true,
+            showTodos = true,
+          },
+        },
+      })
+
+      lsp_config.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      -- Tooltip for the lsp in bottom right
+      require("fidget").setup({})
+
+      -- Hot reload :)
+      require("dart-tools")
+    end,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+
+      { "j-hui/fidget.nvim", tag = "legacy" },
+      -- support for dart hot reload on save
+      "RobertBrunhage/dart-tools.nvim",
+    },
+  },
 }
