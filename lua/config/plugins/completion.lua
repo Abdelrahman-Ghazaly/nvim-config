09a2580ac29502837fc36aa03b75991e92fd24cd
Abdelrahman Ghazaly <aghazaly@Abdelrahmans-MacBook-Pro.local>
2025-02-15 18:45:18 +0200

    Added cpm instead of blink + other chagnes

--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -1,32 +1,96 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
-
-  version = '*',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = 'default',
-      ['<Tab>'] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        'snippet_forward',
-        'fallback'
-      },
-    },
-
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
-
-    signature = { enabled = true }
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets',
+    'onsails/lspkind.nvim',
   },
+  config = function()
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping(function()
+          if not cmp.visible() then
+            cmp.complete()
+          else
+            cmp.abort()
+          end
+        end, { 'i', 's' }),
+
+        ['<C-e>'] = cmp.mapping.close(),
+
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              cmp.confirm({ select = true })
+            end
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+      }),
+      formatting = {
+        format = require('lspkind').cmp_format({
+          mode = 'symbol_text',
+          preset = 'codicons',
+          maxwidth = 120,
+          ellipsis_char = '...',
+          symbol_map = {
+            Text = '󰉿',
+            Method = '󰆧',
+            Function = '󰊕',
+            Constructor = '',
+            Field = '󰜢',
+            Variable = '󰀫',
+            Class = '󰠱',
+            Interface = '',
+            Module = '',
+            Property = '󰜢',
+            Unit = '󰑭',
+            Value = '󰎠',
+            Enum = '',
+            Keyword = '󰌋',
+            Snippet = '',
+            Color = '󰏘',
+            File = '󰈙',
+            Reference = '󰈇',
+            Folder = '󰉋',
+            EnumMember = '',
+            Constant = '󰏿',
+            Struct = '󰙅',
+            Event = '',
+            Operator = '󰆕',
+            TypeParameter = '',
+          },
+        }),
+      },
+      experimental = {
+        ghost_text = true,
+      },
+      view = {
+        entries = { name = 'custom', selection_order = 'near_cursor' }
+      },
+    })
+  end
 }
--- lua/config/plugins/flutter.lua
+++ lua/config/plugins/flutter.lua
@@ -9,23 +9,6 @@ return {
   config = function()
     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    local function toggle_log()
-      local wins = vim.api.nvim_list_wins()
-
-      for _, id in pairs(wins) do
-        local bufnr = vim.api.nvim_win_get_buf(id)
-        if vim.api.nvim_buf_get_name(bufnr):match '.*/([^/]+)$' == '__FLUTTER_DEV_LOG__' then
-          return vim.api.nvim_win_close(id, true)
-        end
-      end
-
-      pcall(function()
-        vim.api.nvim_command 'bot split + __FLUTTER_DEV_LOG__ | resize 15'
-      end)
-    end
-
-    vim.keymap.set("n", "<leader>fl", toggle_log, { desc = "Toggle Flutter dev log" })
-
     require("flutter-tools").setup {
       ui = {
         border = "rounded",
@@ -60,10 +43,7 @@ return {
           end
         end,
       },
-      dev_log = {
-        enabled = true,
-        open_cmd = "bot 15split", -- Changed to open at bottom
-      },
+      dev_log = { enabled = false },
       widget_guides = {
         enabled = false,
       },
